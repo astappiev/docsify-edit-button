2f1ce92d3d057af8acb1c996eb031246668a812d
Oleh Astappiev <astappiev@gmail.com>
2023-04-06 10:32:53 +0000

    feat: print last edited and edit button

---
+++ README.md
@@ -0,0 +1,42 @@
+# docsify-edit-button
+
+A plugin for docsify to add a `Suggest an edit` button on every page, which links to GitHub repository or whenewer else.
+
+## Install
+
+Add the plugin to `index.html` via a CDN or any other way, e.g.:
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/docsify-edit-button"></script>
+```
+
+## Usage
+
+Add javasript to `index.html`, and you can modify the content of the text field arbitrarily:
+
+```html
+<script>
+  window.$docsify = {
+    editButton: {
+      title: "Suggest an Edit",
+      template: "Last modified {docsify-updated}, {edit}",
+      formatUpdated: "{DD}.{MM}.{YYYY}",
+      editBase: "https://github.com/astappiev/docsify-edit-button/edit/main",
+      position: "top", // "top" or "bottom", default to "top"
+    },
+  };
+</script>
+```
+
+## Time Patterns
+
+Please see [https://github.com/lukeed/tinydate#patterns](https://github.com/lukeed/tinydate#patterns)
+
+- `{YYYY}`: full year; eg: **2017**
+- `{YY}`: short year; eg: **17**
+- `{MM}`: month; eg: **04**
+- `{DD}`: day; eg: **01**
+- `{HH}`: hours; eg: **06** (24h)
+- `{mm}`: minutes; eg: **59**
+- `{ss}`: seconds; eg: **09**
+- `{fff}`: milliseconds; eg: **555**
---
+++ index.js
@@ -0,0 +1,41 @@
+(function () {
+  const defaultOptions = {
+    title: "Suggest an Edit 📝",
+    template: "Last modified {docsify-updated}, {edit}",
+    formatUpdated: "{YYYY}/{MM}/{DD}",
+    editBase: undefined,
+    position: "top",
+  };
+
+  function render(vm, options) {
+    const editLink = '<a href="' + options.editBase + "/" + vm.route.file + '">' + options.title + "</a>";
+    return '<div style="overflow: auto"><p style="float: right">' + options.template.replace("{edit}", editLink) + "</p></div>";
+  }
+
+  function plugin(hook, vm) {
+    const options = Object.assign(defaultOptions, vm.config.editButton || {});
+    const whereToPlace = String(options.position).toLowerCase();
+    if (options.formatUpdated) {
+      $docsify.formatUpdated = options.formatUpdated;
+    }
+
+    hook.beforeEach(function (markdown) {
+      if (!options.editBase) {
+        console.error("Please specify `editButton.editBase` link in your docsify config.");
+        return markdown;
+      }
+
+      const text = render(vm, options);
+      switch (whereToPlace) {
+        case "top":
+          return text + "\n\n" + markdown;
+        default:
+          return markdown + "\n\n" + text;
+      }
+    });
+  }
+
+  // Add plugin to docsify's plugin array
+  $docsify = $docsify || {};
+  $docsify.plugins = [].concat($docsify.plugins || [], plugin);
+})();
